cda9af32a883bd74521270da34bd2e4d94feb057
vitoria_cosmos <maria.v.cosmo@gmail.com>
2024-02-14 12:09:17 -0300

    aula 04

--- aulas/app.js
+++ aulas/app.js
@@ -1,15 +1,20 @@
 alert('Boas-vindos ao jogo do número secreto');
-let numeroSecreto = 5;
+// let numeroSecreto = 5;
+// let numeroSecreto = parseInt(Math.random() * 10 + 1);
+
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 
 // enquanto chute não for igual ao número secreto.
 while (chute != numeroSecreto) {
-    chute = prompt('Escolha um número entre 1 e 10');
+    // chute = prompt('Escolha um número entre 1 e 10');
+    chute = prompt('Escolha um número entre 1 e 100');
     // se chute for igual ao número secreto
     if (chute == numeroSecreto) {
-        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+        // PARA não continua
+        break;
     } else {
         // alert('Você errou :(')
         if (chute > numeroSecreto) {
@@ -23,3 +28,16 @@ while (chute != numeroSecreto) {
     
 }
 
+// if (tentativas > 1) {
+//     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+
+// } else {
+//     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
+
+// }
+
+// tentativas > 1 ? alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`) : alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`)
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
